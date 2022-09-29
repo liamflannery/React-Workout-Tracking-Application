785c75573161815540b07e0a7509a8790a081cad
liamflannery <liam.flannery56@gmail.com>
2022-09-29 14:22:50 +1000

    Initial Set Up

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://ilearn.mq.edu.au/mod/page/view.php?id=7131942"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          COMP3120 Individual Project
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
