c44dd4cda48ec8bf9f06b2a8d5552c15f2c9e125
Alexander Lynch <alexander.lynch@students.mq.edu.au>
2022-10-06 11:32:07 +1100

    Individual Day Page Progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
+import Stopwatch from './components/Stopwatch'
 
-function App() {
+function App() { 
+  const [value,setValue]=useState('');
+ const handleSelect=(e)=>{
+    console.log(e);
+    setValue(e)
+ }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://ilearn.mq.edu.au/mod/page/view.php?id=7131942"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          COMP3120 Individual Project
-        </a>
-      </header>
-    </div>
-  );
+
+    <div className="App container">
+      <Stopwatch></Stopwatch>
+      <DropdownButton
+      
+      title="Day 1 Workout"
+      id="dropdown-menu-align-right"
+     onSelect={handleSelect}
+      >
+      
+            <Dropdown.Item eventKey="Bench Press">Bench Press</Dropdown.Item>
+            <Dropdown.Item eventKey="Incline Bench Press">Incline Bench Press</Dropdown.Item>
+            
+            <Dropdown.Item eventKey="Cable Fly's">Cable Fly's</Dropdown.Item>
+              
+            <Dropdown.Item eventKey="Overhead Press">Overhead Press</Dropdown.Item>
+            
+      </DropdownButton>
+      <h4> {value}</h4> 
+      </div>
+      
+    
+  
+
+  
+);
 }
 
 export default App;
