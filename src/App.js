389f20d25f258980089e74927b7ec0095964d2f3
Alexander Lynch <alexander.lynch@students.mq.edu.au>
2022-10-11 18:42:35 +1100

    Progress

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import React,{useState} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown'
-import Stopwatch from './components/Stopwatch'
+import './App.css';
+import Stopwatch from './components/Stopwatch';
+import WorkoutPage from './pages/WorkoutPage'
 
-function App() { 
-  const [value,setValue]=useState('');
- const handleSelect=(e)=>{
-    console.log(e);
-    setValue(e)
- }
-
-
+function App() {
+  
   return (
 
-    <div className="App container">
+    <div className="App">
+       
       <Stopwatch></Stopwatch>
-      <DropdownButton
-      
-      title="Day 1 Workout"
-      id="dropdown-menu-align-right"
-     onSelect={handleSelect}
-      >
-      
-            <Dropdown.Item eventKey="Bench Press">Bench Press</Dropdown.Item>
-            <Dropdown.Item eventKey="Incline Bench Press">Incline Bench Press</Dropdown.Item>
-            
-            <Dropdown.Item eventKey="Cable Fly's">Cable Fly's</Dropdown.Item>
-              
-            <Dropdown.Item eventKey="Overhead Press">Overhead Press</Dropdown.Item>
-            
-      </DropdownButton>
-      <h4> {value}</h4> 
+      <WorkoutPage/>
+  
       </div>
       
-    
-  
+     );
+     
 
-  
-);
 }
-
 export default App;
