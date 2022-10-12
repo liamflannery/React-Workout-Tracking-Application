de10fb5b32d6f9a78e6cb6da5b0a028f1d452572
Groobin <79952458+Groobin@users.noreply.github.com>
2022-10-13 03:35:43 +1100

    test daydata

--- src/components/daycontent.js
+++ src/components/daycontent.js
@@ -2,6 +2,66 @@ import React from 'react'
 import { Divider, Header, Image, Segment } from 'semantic-ui-react'
 import {Link} from "react-router-dom";
 
+const dayData = [
+  { 
+    id: 1,
+    name: "Monday",
+    workout: [
+      "Bench Press",
+      "Deadlift"
+    ]
+  },
+  { 
+    id: 2,
+    name: "Tuesday",
+    workout: [
+      "Barbell Squats"
+    ] 
+  },
+  { 
+    id: 3,
+    name: "Wednesday",
+    workout: [
+      "Pull-Ups",
+    ]
+  },
+  { 
+    id: 4,
+    name: "Thursday",
+    workout: [
+      "Seated Cable Rows" 
+    ]
+  },
+  { 
+    id: 5,
+    name: "Friday",
+    workout: [
+      "Bicep Curls"
+    ]
+  },
+];
+
+const [days, setDays] = useState([]);
+
+useEffect(() => {
+  setDays(dayData);
+}, []);
+
+const handleChange = (e) => {
+  const { name, checked } = e.target;
+  if (name === "allSelect") {
+    let tempDay = days.map((day) => {
+      return { ...day, isChecked: checked };
+    });
+    setDays(tempDay);
+  } else {
+    let tempDay = days.map((day) =>
+    day.name === name ? { ...day, isChecked: checked } : day
+    );
+    setDays(tempDay);
+  }
+};
+
 const daycontent = (title) => (
   <Segment>
     <Link to="/day">
