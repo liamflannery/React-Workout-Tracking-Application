1811397262cc4b8ac4075b74c9cf68f7d27bb8dc
Alexander Lynch <alexander.lynch@students.mq.edu.au>
2022-10-18 19:52:26 +1100

    update

---
+++ src/pages/Excercise.js
@@ -0,0 +1,65 @@
+export default [
+    {
+      id: 10,
+      Workout: "Bench Press",
+     
+      projects: [
+        {
+          id: "1",
+          weight: "Weight: 75kg"
+        },
+        {
+          id: "2",
+          weight: "Weight: 75kg"
+        },
+        {
+          id: "3",
+          weight: "Weight: 75kg"
+        }
+      ],
+    
+      
+    },
+    {
+      id: 20,
+      Workout: "Incline Bench Press",
+      
+      projects: [
+        {
+          id: "1",
+          weight: "Weight: 50kg"
+        },
+        {
+          id: "2",
+          weight: "Weight: 50kg"
+        },
+        {
+          id: "3",
+          weight: "Weight: 50kg"
+        }
+      ],
+     
+    },
+    {
+      id: 30,
+      Workout: "Shoulder Press",
+      
+      projects: [
+        {
+          id: "1",
+          weight: "Weight: 40kg"
+        },
+        {
+          id: "2",
+          weight: "Weight: 40kg"
+        },
+        {
+          id: "3",
+          weight: "Weight: 40kg"
+        }
+      ],
+     
+      
+    }
+  ];
+  
