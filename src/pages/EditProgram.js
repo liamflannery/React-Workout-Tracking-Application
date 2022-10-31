194bc5fd82f091b6ddcd2b1a4fe2ba0fefa449bd
liamflannery <liam.flannery56@gmail.com>
2022-10-31 16:54:31 +1100

    Seperate file for component

--- src/pages/EditProgram.js
+++ src/pages/EditProgram.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { v4 as uuid } from 'uuid';
-
+import EditBox from "./EditBox";
 const itemsFromBackend = [
     {
       id: uuid(),
@@ -66,65 +66,7 @@ const onDragEnd = (result, columns, setColumns) => {
 
 function EditProgram() {
   const [columns, setColumns] = useState(columnsFromBackend);
-  const EditBox = (props) => {
-    console.log(props.column.name)
-    return (
-        
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center"
-          }}
-          key={[props.columnId]}
-        >
-          <h2>{props.column.name}</h2>
-          <div style={{ margin: 8 }}>
-            <Droppable droppableId={props.columnId} key={props.columnId}>
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    className="exercises"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{
-                      background: snapshot.isDraggingOver
-                        ? "lightblue"
-                        : "lightgrey",
-        
-                    }}
-                  >
-                    {props.column.items.map((item, index) => {
-                      return (
-                        <Draggable
-                          key={item.id}
-                          draggableId={item.id}
-                          index={index}
-                        >
-                          {(provided, snapshot) => {
-                            return(
-                            <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                            <div className="exercises-thumb">
-                                <img src={item.thumb} alt={`${item.name} Thumb`} />
-                            </div>
-                            <p>
-                                { item.name }
-                            </p>
-                            </li>
-                            )
-                          }}
-                        </Draggable>
-                      );
-                    })}
-                    {provided.placeholder}
-                  </div>
-                );
-              }}
-            </Droppable>
-          </div>
-        </div>
-      );
-  }
+  
   return (
     <div class = "container">
     <header className="App-header">
