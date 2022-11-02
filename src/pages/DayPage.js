bde57b82d771cacc04fb31a8b47c190ed7430869
liamflannery <liam.flannery56@gmail.com>
2022-11-03 10:44:21 +1100

    Added daypage changes

--- src/pages/DayPage.js
+++ src/pages/DayPage.js
@@ -1,137 +1,164 @@
+import React, { useState,useEffect} from 'react';
+//import ReactDOM from 'react-dom/client'
+//import Excercises from "./Excercise";
+import dayService from '../services/days';
+import Collapsible from 'react-collapsible';
+//import axios from "axios";
+import  "./DayPage.css";
 
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-import React, { useState} from 'react';
-import { Component } from "react";
-import Excercises from "./Excercise";
+const DayPage =() => {
 
-
-class DayPage extends Component {
+  const columns = [
+    {
+      title: "Exercises",
+      key: "exercises"
+    },
+    {
+      title: "Reps",
+      key: "reps"
+    },
+    {
+      title: "Weight",
+      key: "weight"
+    }
    
-  constructor(props) {
-    super(props);
-    this.state = { expandedRows: [] };
-  }
-
-  handleExpand = Excercise => {
-    let newExpandedRows = [...this.state.expandedRows];
-    let allExpanded = this.state.allExpanded;
-    let idxFound = newExpandedRows.findIndex(id => {
-      return id === Excercise.id;
-    });
-
-    if (idxFound > -1) {
-      console.log("Collapsing " + Excercise.Workout + " " + idxFound);
-      newExpandedRows.splice(idxFound, 1);
-    } else {
-      console.log("Expanding " + Excercise.Workout);
-      newExpandedRows.push(Excercise.id);
-    }
-
-    console.log("Expanded rows");
-    console.log(newExpandedRows);
-
-    this.setState({ expandedRows: [...newExpandedRows] });
-  };
-
-  isExpanded = Excercise => {
-    const idx = this.state.expandedRows.find(id => {
-      return id === Excercise.id;
-    });
-
-    return idx > -1;
-  };
-
-  expandAll = Excercises => {
-    console.log("ExapndedRows: " + this.state.expandedRows.length);
-    console.log("Excercises:      " + Excercises.length);
-    if (this.state.expandedRows.length === Excercises.length) {
-      let newExpandedRows = [];
-      this.setState({ expandedRows: [...newExpandedRows] });
-      console.log("Collapsing all...");
-    } else {
-      let newExpandedRows = Excercises.map(Excercise => Excercise.id);
-      this.setState({ expandedRows: [...newExpandedRows] });
-      console.log("Expanding all...");
-      console.log("Expanded rows " + newExpandedRows.length);
-    }
-  };
-
-  getRows = Excercise => {
-    let rows = [];
-    const projects = Excercise.projects || [];
-
-    const firstRow = (
-      <tr>
-        <td>{Excercise.Workout}</td>
-        
-        <td>
-          {projects.length > 0 && (
-            <button onClick={() => this.handleExpand(Excercise)}>
-              {this.isExpanded(Excercise) ? "-" : "+"}
-            </button>
-          )}
-        </td>
-      </tr>
-    );
-
-    rows.push(firstRow);
-
-    if (this.isExpanded(Excercise) && projects.length > 0) {
-      const projectRows = projects.map(project => (
-        <tr className="Excercise-details">
-          <td className="Excercise-details" />
-          <td colspan="3" className="Excercise-details">
-            <br />
-            <div className="attribute">
-              <div className="attribute-name">Reps: 8 </div>
-             
-
-              <div className="attribute-value">{project.weight}</div>
-              <input type="checkbox" />
-            </div>
-            <br />
-          </td>
-        </tr>
-      ));
-
-      rows.push(projectRows);
-    }
-
-    return rows;
-  };
-
-  getExcerciseTable = Excercises => {
-    const ExcerciseRows = Excercises.map(Excercise => {
-      return this.getRows(Excercise);
-    });
-
-    return (
-     
+  ];
+  const data = [
+    {
+      id:1,
+      exercises: "Bench Press",
+      reps: "8",
       
-      <table className="my-table">
-        <h1>Day 1</h1>
-        <tr>
-          <th>Excercises</th>
+      weight: "70kg",
+         
+     
+    },
+    { id:2,
+      exercises: "Leg Press",
+      reps: "6",
+      weight: "140kg",
+      child: [
+        {
+          excercises: "",
+          reps: "6",
+          weight: "140kg"
+        },
+        {
+          excercises: "",
+          reps: "6",
+          weight: "160kg"
+        },
+      ]
+    },
+        { 
+          id:3,
+          exercises: "Squats",
+          reps: "8",
+          weight: "80kg",
+          input: "checkbox",
+          child: [
+            {
+              exercises: "",
+              reps: "6",
+              weight: "80kg"
+            },
+            {
+              exercises: "",
+              reps: "4",
+              weight: "100kg"
+            },
           
-          
-          <th onClick={() => this.expandAll(Excercises)}>
-            <button>
-              {Excercises.length === this.state.expandedRows.length ? "-" : "+"}
-            </button>
-          </th>
-        </tr>
-        {ExcerciseRows}
-      </table>
-    );
-  };
-
-  render() {
-    return <div>{this.getExcerciseTable(Excercises)}</div>;
+          ]
+    },
+  
+  { id:4,
+    exercises: "Incline Bench Press",
+    reps: "10",
+    weight: "50kg",
+    child: [
+      {
+        exercises: "",
+        reps: "8",
+        weight: "55kg"
+      },
+      {
+        exercises: "",
+        reps: "6",
+        weight: "60kg"
+      },
+    ]
   }
+];
+
+
+  const [day, setDayName] = useState()
+   
+     
+   const getCurrentDay = async() => {
+   
+        
+      const dayParam = await dayService.getDay(0)
+    setDayName(dayParam.day.day)
+    console.log("DAY PARAM");
+    console.log(dayParam.day.day)
+  
+       
+     
+
+   }
+  
+   useEffect(() => {
+     getCurrentDay();
+     
+
+   },[]);
+   
+     return (
+       <div className="container">
+           <h1>Day 1 Workout</h1>
+
+         <table>
+      
+         
+           <tr>
+             <th>Exercise</th>
+             <th>Weight</th>
+             <th>Reps</th>
+           </tr>
+          
+           {data.map((title, key) => {
+             return (
+              <Collapsible trigger="Show Exercise ">  
+               <tr key={key}>
+              
+                 <th>{title.exercises}
+                
+                 </th>
+                 <th>{title.weight}
+                 <p>80kg</p>
+                 <p>80kg</p>
+                 </th>
+                 <th>{title.reps}
+                <p>6</p>
+                <p>6</p>
+                
+                <input type="checkbox" />Complete
+      </th>
+
+                 
+                
+    
+               </tr>
+                </Collapsible> 
+             )
+           })}
+            
+         
+         </table>
+       
+         </div>
+   
+     );
+   
 }
-
-
-
-
-export default DayPage
+   export default DayPage;
